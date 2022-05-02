6acc8790dcdef2758ae449f5795d584b33a04553
Cowbon <alexalex850520@gmail.com>
2022-05-02 12:57:25 -0700

    add shot classifier

--- ad_detector/shotdetector.py
+++ ad_detector/shotdetector.py
@@ -1,26 +1,74 @@
-from dataclasses import dataclass
+import os
+from dataclasses import dataclass, field, asdict
+import json
 
-# Standard PySceneDetect imports:
+from tqdm import tqdm
+import yaml
+import numpy as np
+import ffmpeg
 from scenedetect import VideoManager, SceneManager
 from scenedetect.detectors import AdaptiveDetector
 
 
 @dataclass
 class Shot:
+    sequence: int
     start_frame: int
     end_frame: int
     start_timestamp: float
     end_timestamp: float
+    features: dict = field(default_factory=lambda : {})
+    test_is_ad: bool = False
+    is_ad: bool = None
+    
+    @property
+    def duration(self):
+        return self.end_timestamp - self.start_timestamp
+    
     
 
 class ShotDetector:
-    def __init__(self, input_video, input_audio):
-        self._input_path = './dataset/Videos/data_test1.mp4'
-        self.threshold = 2.0
-    
-    def detect(self):
+    def __init__(self, input_video):
+        self.input_video = input_video
+        self._mp4_path = input_video[:-3] + 'mp4'
+        self.threshold = 3.0
+        self._preprocess_video()
+
+    def _preprocess_video(self):
+        """ Convert .rgb and .wav file to .mp4 format for scenedetect API"""
+        if os.path.exists(self._mp4_path):
+            print('mp4 file already exists')
+            return
+        
+        with open("format.yaml") as file:
+            format = yaml.safe_load(file)
+            height = format['video_height']
+            width = format['video_width']
+            fps = format['video_fps']
+        
+        print('Start converting...')
+        raw_bytes = np.fromfile(self.input_video, np.dtype('B'))
+        n_frames = len(raw_bytes)//(height*width*3)
+        frames = raw_bytes.reshape((n_frames, 3, height, width))
+        frames = np.moveaxis(frames, 1, -1)  # pack rgb values per pixel
+        
+        process = (
+            ffmpeg
+            .input('pipe:', format='rawvideo', pix_fmt='rgb24', s=f'{width}x{height}', framerate=fps)
+            .output(self._mp4_path, pix_fmt='yuv420p', vcodec="libx264")
+            .overwrite_output()
+            .run_async(pipe_stdin=True, overwrite_output=True, pipe_stderr=True)
+        )
+
+        for frame in tqdm(frames):
+            process.stdin.write(frame.astype(np.uint8).tobytes())
+            
+        process.stdin.close()
+        process.wait()
+
+    def detect(self, save_json=False):
         # Create our video & scene managers, then add the detector.
-        video_manager = VideoManager([self._input_path])
+        video_manager = VideoManager([self._mp4_path])
         scene_manager = SceneManager()
         scene_manager.add_detector(AdaptiveDetector(video_manager, 
                                                     adaptive_threshold=self.threshold))
@@ -35,18 +83,22 @@ class ShotDetector:
         # Each returned scene is a tuple of the (start, end) timecode.
         shots = []
         for (timecode_start, timecode_end) in scene_manager.get_scene_list():
-            shots.append(Shot(start_frame = timecode_start.get_frames(),
+            shots.append(Shot(sequence = len(shots),
+                              start_frame = timecode_start.get_frames(),
                               end_frame = timecode_end.get_frames(),
                               start_timestamp = timecode_start.get_seconds(),
                               end_timestamp = timecode_end.get_seconds()))
-        breakpoint()
+        
+        if save_json:
+            print('Saving to json')
+            json_list = [asdict(shot) for shot in shots]
+            with open('./shot-result/new-dataset.json', 'w') as file:
+                json.dump(json_list, file)
+            
+        
         return shots
     
-    def debug(self):
-        import json
-        s = '[{"start_frame": 0, "end_frame": 606, "start_timestamp": 0.0, "end_timestamp": 20.2}, {"start_frame": 606, "end_frame": 1179, "start_timestamp": 20.2, "end_timestamp": 39.3}, {"start_frame": 1179, "end_frame": 2400, "start_timestamp": 39.3, "end_timestamp": 80.0}, {"start_frame": 2400, "end_frame": 2435, "start_timestamp": 80.0, "end_timestamp": 81.16666666666667}, {"start_frame": 2435, "end_frame": 2489, "start_timestamp": 81.16666666666667, "end_timestamp": 82.96666666666667}, {"start_frame": 2489, "end_frame": 2515, "start_timestamp": 82.96666666666667, "end_timestamp": 83.83333333333333}, {"start_frame": 2515, "end_frame": 2596, "start_timestamp": 83.83333333333333, "end_timestamp": 86.53333333333333}, {"start_frame": 2596, "end_frame": 2659, "start_timestamp": 86.53333333333333, "end_timestamp": 88.63333333333334}, {"start_frame": 2659, "end_frame": 2695, "start_timestamp": 88.63333333333334, "end_timestamp": 89.83333333333333}, {"start_frame": 2695, "end_frame": 2748, "start_timestamp": 89.83333333333333, "end_timestamp": 91.6}, {"start_frame": 2748, "end_frame": 2803, "start_timestamp": 91.6, "end_timestamp": 93.43333333333334}, {"start_frame": 2803, "end_frame": 2838, "start_timestamp": 93.43333333333334, "end_timestamp": 94.6}, {"start_frame": 2838, "end_frame": 3630, "start_timestamp": 94.6, "end_timestamp": 121.0}, {"start_frame": 3630, "end_frame": 4350, "start_timestamp": 121.0, "end_timestamp": 145.0}, {"start_frame": 4350, "end_frame": 5550, "start_timestamp": 145.0, "end_timestamp": 185.0}, {"start_frame": 5550, "end_frame": 5582, "start_timestamp": 185.0, "end_timestamp": 186.06666666666666}, {"start_frame": 5582, "end_frame": 5612, "start_timestamp": 186.06666666666666, "end_timestamp": 187.06666666666666}, {"start_frame": 5612, "end_frame": 5699, "start_timestamp": 187.06666666666666, "end_timestamp": 189.96666666666667}, {"start_frame": 5699, "end_frame": 5753, "start_timestamp": 189.96666666666667, "end_timestamp": 191.76666666666668}, {"start_frame": 5753, "end_frame": 5846, "start_timestamp": 191.76666666666668, "end_timestamp": 194.86666666666667}, {"start_frame": 5846, "end_frame": 5925, "start_timestamp": 194.86666666666667, "end_timestamp": 197.5}, {"start_frame": 5925, "end_frame": 5987, "start_timestamp": 197.5, "end_timestamp": 199.56666666666666}, {"start_frame": 5987, "end_frame": 6450, "start_timestamp": 199.56666666666666, "end_timestamp": 215.0}, {"start_frame": 6450, "end_frame": 7200, "start_timestamp": 215.0, "end_timestamp": 240.0}, {"start_frame": 7200, "end_frame": 9000, "start_timestamp": 240.0, "end_timestamp": 300.0}]'
-        shots = []
-        for shot_dict in json.loads(s):
-            shots.append(Shot(**shot_dict))
-        return shots
-        
+    def from_json(self, i):
+        with open(f'./shot-result/dataset{i}.json') as file:
+            shot_raw = json.load(file)
+        return [ Shot(**shot_dict) for shot_dict in shot_raw ]
--- myprogram.py
+++ myprogram.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
+
+print('Loading packages...')
+
 import click
 
 from ad_detector.shotdetector import ShotDetector
 from ad_detector.featurebuilder import FeatureBuilder
+from ad_detector.shotclassifier import ShotClassifier
 
 @click.command()
 @click.argument('input_video', type=click.Path(exists=True))
 @click.argument('input_audio', type=click.Path(exists=True))
-def main(input_video, input_audio):
-    shot_detector = ShotDetector(input_video, input_audio)
-    # shots = shot_detector.detect()
-    shots = shot_detector.debug()
+@click.option('-d', '--dataset', 'dataset', type=int, default=None)
+def main(input_video, input_audio, dataset):
+    print('Start detecting...')
+    shot_detector = ShotDetector(input_video)
+    if dataset is None:
+        shots = shot_detector.detect(save_json=True)
+    else:
+        shots = shot_detector.from_json(dataset)
+    print('Detecting done')
     
     feature_builder = FeatureBuilder(shots, input_video, input_audio)
     feature_builder.build()
+    
+    shot_classifier = ShotClassifier(shots)
+    shot_classifier.classify()
+    shot_classifier.plot()
+    
+    
 
 if __name__ == '__main__':
     main()
